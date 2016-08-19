30ad7b5d735d6c9b03727cef89e3c71cb6c9d291
aleksander-zn <aleksander-zn@users.noreply.github.com>
2016-08-19 02:35:49 +0200

    Improve bootloader/make.sh

--- bootloader/make.sh
+++ bootloader/make.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -x
+
 cat 01.c
-[ -f 01.o ] && rm 01.o
+
+[ -f 01.o   ] && rm 01.o
 [ -f 01.lst ] && rm 01.lst
-avr-gcc -mmcu=atmega328p -Os -Wall -g 01.c -c -o 01.o
-if [ -f 01.o ]
-then
-	avr-objdump -xS 01.o > 01.lst
-	rm 01.o
-	cat 01.lst
-fi
+
+avr-gcc -mmcu=atmega328p -Os -Wall -g 01.c -c -o 01.o || exit $?
+
+avr-objdump -xS 01.o > 01.lst
+
+rm 01.o
+
+cat 01.lst
